dbef9b6efb8b8c1b9f982a22b25d030cafd39474
Terin Sebastin <terin01@gmail.com>
2023-05-22 05:24:54 +0100

    Added new table and data using Migrations

---
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -1,11 +1,19 @@
+
+using Mango.Services.ProductAPI.DbContexts;
+using Microsoft.EntityFrameworkCore; 
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+                options.UseSqlServer(connectionString));
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
