81fa83de3ff6ed96fc58000c3eed9c48e078464b
Terin Sebastin <terin01@gmail.com>
2023-05-23 03:23:14 +0100

    Implementing Product repository

---
+++ Mango.Services.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.DbContexts;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDBContext _db;
+        private readonly IMapper _mapper;
+
+        public ProductRepository(ApplicationDBContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public Task<ProductDto> CreateUpdateProduct(ProductDto product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ProductDto> GetProductById(int id)
+        {
+            Product product = await _db.Products.FirstOrDefaultAsync(x=> x.ProductId == id);
+            return _mapper.Map<ProductDto>(product);
+        } 
+
+        public async Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            List<Product> productList = await _db.Products.ToListAsync();
+            return _mapper.Map<List<ProductDto>>(productList);    
+        }
+    }
+}
